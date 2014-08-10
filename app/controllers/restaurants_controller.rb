835f15311b776769c4bd176d151eb11363471098
Emma <emma_williams1@hotmail.co.uk>
2014-08-10 17:15:20 +0100

    now able to add images for restaurants need to work on resizing and heroku

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -69,6 +69,7 @@ class RestaurantsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :phone, :website)
+      params.require(:restaurant).permit(:name, :address, :phone, :website, :image)
+      #this is for safety and security reasons
     end
 end
