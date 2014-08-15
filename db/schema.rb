dd0370e914ffb597b7836f8bd8576b23274eb87f
Emma <emma_williams1@hotmail.co.uk>
2014-08-15 11:20:29 +0100

    add reviews form styled

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813100630) do
+ActiveRecord::Schema.define(version: 20140814161619) do
 
   create_table "models", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 20140813100630) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.string   "rating"
+    t.string   "integer"
+    t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
