937df9fa9ec9a91077910754720825e19d06a2a1
Emma <emma_williams1@hotmail.co.uk>
2014-08-31 14:38:10 +0100

    added validations for phone using regexp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,6 @@ class Restaurant < ActiveRecord::Base
     has_many :reviews
 
     validates :name, :address, :phone, :website, :image, presence: :true
+    validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+    message: "must be in the format (123) 456-7890" }
 end
